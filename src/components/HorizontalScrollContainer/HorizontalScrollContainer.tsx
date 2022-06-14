d660be6e835acb6b6fb69c43327f5e46d2b4431b
Matthew Boyea <yourzombiemop@gmail.com>
2022-06-14 09:32:43 -0500

    container scrolling prototyped

--- src/components/HorizontalScrollContainer/HorizontalScrollContainer.tsx
+++ src/components/HorizontalScrollContainer/HorizontalScrollContainer.tsx
@@ -1,16 +1,28 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, useRef} from 'react';
 
 import './HorizontalScrollContainer.sass'
 
 interface HorizontalScrollContainerProps {
   children?: React.ReactNode;
+  scrollStep: Number;
 }
 
 const HorizontalScrollContainer: FC<HorizontalScrollContainerProps> = ({
   children,
 }): ReactElement => {
+  const containerRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  const onScroll: React.WheelEventHandler<HTMLDivElement> = (e) => {
+    if (e.deltaX == 0 && e.deltaY != 0) {
+      console.log(e);
+      containerRef.current.scroll({
+        top: 0,
+        left: containerRef.current.scrollLeft + e.deltaY,
+        behavior: 'smooth'
+      });
+    }
+  }
   return (
-    <div className='horizontal-scroll-container'>
+    <div ref={containerRef} onWheel={onScroll} className='horizontal-scroll-container'>
       {children}
     </div>
   );
